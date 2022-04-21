538d2ef8accfcf6e09361c170f7100bfffd98e6f
kevlampiev <kevlampiev@gmail.com>
2022-04-21 12:21:52 +0700

    Добавление класса дерева каталогов

---
+++ KEvlampiev.TerminalForms/DirectoryViewWindow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEvlampiev.TerminalForms
+{
+    internal class DirectoryViewWindow:ConsoleWindow
+    {
+
+        //Поле того, чтобы понять, какая поддиректория/файл отображается на самом верху экрана окна
+        private int _startDirectoryIndex = 0;
+
+        public string CurrentDirectory { get; set; }
+        
+
+        public DirectoryViewWindow(int left, int top, int width, int height, string title, string currentDir):base(left,top,width,height,title)
+        { 
+            CurrentDirectory = currentDir;
+        }
+
+        public override void Repaint()
+        {
+            base.Repaint();
+
+
+        }
+
+        //Вспомогательная функция, которая выдает все подкаталоги текущего каталога 
+        private string[] GetTree(string[] tree, DirectoryInfo dir) 
+        {
+            return new string[1];
+        }
+
+        //Вспомогательная функция. Заполняет внутренность окна 
+        private void DrawDirectoryTree() 
+        {
+            
+        }
+    }
+}
